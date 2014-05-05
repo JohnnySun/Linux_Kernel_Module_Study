fc7f2695bd030bd4d6afe666561ee93e638178b7
JohnnySun <bmy001@gmail.com>
2014-05-05 12:25:01 +0800

    Add Function: cpoy_to_user

--- hello/hello.c
+++ hello/hello.c
@@ -18,6 +18,11 @@ struct scull_dev {
 		struct cdev cdev; /* Char device structure */
 };
 
+struct scull_qset {
+		void **data;
+		struct scull_qset *next;
+};
+
 struct file_operations scull_fops = {
 		.owner = THIS_MODULE,
 		.llseek = scull_llseek,
@@ -29,6 +34,28 @@ struct file_operations scull_fops = {
 };
 
 
+int scull_trim(struct scull_dev *dev)
+{
+		struct scull_qset *next, *dptr;
+		int qset = dev->qset; /* "dev" is not-null */
+		int i;
+		for (dptr = dev->data; dptr; dptr = next)
+		{ /* all the list items */
+				if (dptr->data) {
+						for (i = 0; i < qset; i++)
+								kfree(dptr->data[i]);
+						kfree(dptr->data);
+						dptr->data = NULL;
+				}
+				next = dptr->next;
+				kfree(dptr);
+		}
+		dev->size = 0;
+		dev->quantum = scull_quantum;
+		dev->qset = scull_qset;
+		dev->data = NULL;
+		return 0;
+}
 
 
 static void scull_setup_cdev(struct scull_dev *dev, int index)
@@ -59,6 +86,45 @@ int scull_open(struct inode *inode, struct file *filp)
 int scull_release(struct inode *inode, struct file *filp)
 {
 		return 0;
+}
+
+ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
+{
+		struct scull_dev *dev = filp->private_data;
+		struct scull_qset *dptr; /* the first listitem */
+		int quantum = dev->quantum, qset = dev->qset;
+		int itemsize = quantum * qset; /* how many bytes in the listitem */
+		int item, s_pos, q_pos, rest;
+		ssize_t retval = 0;
+		if (down_interruptible(&dev->sem))
+				return -ERESTARTSYS;
+		if (*f_pos >= dev->size)
+				goto out;
+		if (*f_pos + count > dev->size)
+				count = dev->size - *f_pos;
+		/* find listitem, qset index, and offset in the quantum */
+		item = (long)*f_pos / itemsize;
+		rest = (long)*f_pos % itemsize;
+		s_pos = rest / quantum;
+		q_pos = rest % quantum;
+		/* follow the list up to the right position (defined elsewhere) */
+		dptr = scull_follow(dev, item);
+		if (dptr == NULL || !dptr->data || ! dptr->data[s_pos])
+				goto out; /* don't fill holes */
+		/* read only up to the end of this quantum */
+		if (count > quantum - q_pos)
+				count = quantum - q_pos;
+		if (copy_to_user(buf, dptr->data[s_pos] + q_pos, count))
+		{
+				retval = -EFAULT;
+				goto out;
+		}
+		/* copy data to user space */
+		*f_pos += count;
+		retval = count;
+out:
+		up(&dev->sem);
+		return retval;
 }
 
 
